203512ffef27d8528cff0bb8d237b6f3dea27b6d
VIVEK <vivekbalani1@gmail.com>
2022-12-14 21:41:01 +0530

    Added Knight.java class and added the List for Leagal moves

--- src/com/chess/engine/piece/Piece.java
+++ src/com/chess/engine/piece/Piece.java
@@ -16,6 +16,10 @@ public abstract class Piece {
         this.pieceAlliance = pieceAlliance;
     }
 
+    public Alliance getPieceAlliance() {
+        return this.pieceAlliance;
+    }
+
     //Instead a list set could be better because moves are unordered
     //But for simplicity lets make a list
     public abstract List<Move> calculateLegalMoves(final Board board);
