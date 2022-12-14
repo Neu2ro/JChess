58d81ec6345f6a00d81619d3fff5797ab30bd36d
VIVEK <vivekbalani1@gmail.com>
2022-12-14 21:13:51 +0530

    classes for leagal moves added withot methods

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -8,7 +8,7 @@ public abstract class Tile {
 
     protected final int tileCoordinate;
 
-    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
@@ -21,7 +21,7 @@ public abstract class Tile {
     }
 
     public static Tile createTile(final int tileCoordinate, final Piece piece) {
-        return piece != null ? new EmptyTile.OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece != null ? new EmptyTile.OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
     private Tile(int tileCoordinate) {
@@ -34,7 +34,7 @@ public abstract class Tile {
 
     public static final class EmptyTile extends Tile{
 
-        EmptyTile(final int coordinate) {
+        private EmptyTile(final int coordinate) {
             super(coordinate);
         }
 
@@ -52,7 +52,7 @@ public abstract class Tile {
 
             private final Piece pieceOnTile;
 
-            OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            private OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
                 super(tileCoordinate);
                 this.pieceOnTile = pieceOnTile;
             }
